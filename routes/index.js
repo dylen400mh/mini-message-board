b878ebc37f17296ad1a3baf4e5c44ef566f30720
dylen400mh <dylenbelanger2004@gmail.com>
2024-07-04 22:14:27 -0400

    Move /new routes into index.js

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+// New message routes
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
 module.exports = router;
